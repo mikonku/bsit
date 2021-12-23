70460209f95b283f6ce5a56946946ae686bb10f7
mikonku <septiawanr318@gmail.com>
2021-12-23 22:56:05 +0700

    delete unimportant files

--- ci/todo/app/Views/template/index.php
+++
@@ -1,8 +0,0 @@
-<?php 
-echo view("template/head.php");
-echo view("template/navbar.php");
-echo view("template/sidebar.php");
-echo view("template/content.php");
-echo view("template/footer.php");
-?>
- 
